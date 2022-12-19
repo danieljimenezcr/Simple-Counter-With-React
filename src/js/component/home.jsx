9b882791aeef78496b726656c47ec805a83bc986
Daniel Jimenez <daniel.jimenez.cr@gmail.com>
2022-12-19 03:48:18 +0000

    Add files to repository

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom"
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main-container">
+			<div className="counter">
+				<div className="clock">
+					<i className="fa-regular fa-clock"></i>
+				</div>
+				<div className="six">{props.digit6}</div>
+				<div className="five">{props.digit5}</div>
+				<div className="four">{props.digit4}</div>
+				<div className="three">{props.digit3}</div>
+				<div className="two">{props.digit2}</div>
+				<div className="one">{props.digit1}</div>
+			</div>
+
 		</div>
 	);
 };
 
-export default Home;
+Home.propTypes = {
+	digit1: PropTypes.number,
+	digit2: PropTypes.number,
+	digit3: PropTypes.number,
+	digit4: PropTypes.number,
+	digit5: PropTypes.number,
+	digit6: PropTypes.number,
+}
+
+let counter = 0;
+
+setInterval(function () {
+
+	const six = Math.floor(counter / 100000 % 10);
+	const five = Math.floor(counter / 10000 % 10);
+	const four = Math.floor(counter / 1000 % 10);
+	const three = Math.floor(counter / 100 % 10);
+	const two = Math.floor(counter / 10 % 10);
+	const one = Math.floor(counter / 1 % 10);
+
+	counter++
+
+ReactDOM.render(<Home digit1 = {one} digit2 = {two} digit3 = {three} digit4 = {four} digit5 = {five} digit6 = {six}  />, document.querySelector("#app"));
+},1000)
+
+
+
